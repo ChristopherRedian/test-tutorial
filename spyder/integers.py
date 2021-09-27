faff106e6d12742366b6153cf9c64bcfb1183f20
Christopher Redian <credian001@mymail.sim.edu.sg>
2021-09-27 15:21:49 +0700

    my first practice assignment

---
+++ spyder/integers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 15:02:36 2021
+
+@author: Christopher Redian
+"""
+
+#%%
+print(1,2,3,4,5,6,7,8,9,10)
+
+
+#%%
+for i in range(1,11):
+        print(i)
+        
